c32d3f87d8aac73fe539a2ce7b6dc74ffb86221d
Peter Benson <pbenson@mbta.com>
2022-03-10 11:34:44 -0500

    added basic query class

---
+++ query_class.py
@@ -0,0 +1,13 @@
+
+
+
+class Query:
+    def __init__(self) -> None:
+        # Parameters for this:
+        # - DL path
+        # - Query Name (ex. PL_PO_ENCUMB)
+        # - Query type (ex. direct_query)
+        # - Query Parameters (ex. date ranges)
+        # - Query Schedule
+
+        pass
